ab16b86df2eba1bb89c5313b69293857075c8f77
MaikoDev <phyonaung@ymail.com>
2024-06-12 20:00:12 -0700

    Added method to retrieve single pixel and transparency from IRenderable

--- src/main/java/org/maikodev/rendering/IRenderable.java
+++ src/main/java/org/maikodev/rendering/IRenderable.java
@@ -4,6 +4,9 @@ public interface IRenderable {
     char[] getPixelBuffer();
     boolean[] getTransparencyBuffer();
 
+    char getPixel(int rowMajorIndex);
+    boolean isPixelVisible(int rowMajorIndex);
+
     int getBufferWidth();
     int getBufferHeight();
 }
